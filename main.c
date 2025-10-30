879fecb620127a01f796e1d51a6e324bc24bfd7d
sjgift085 <77606778+sjgift085@users.noreply.github.com>
2025-10-30 15:27:34 +0900

    실습 3-2

--- main.c
+++ main.c
@@ -4,19 +4,20 @@
 
 int main(int argc, char *argv[])
 {
-    int i,average;
-		int sum; 
-    int grade[SIZE];
-    int score[SIZE];
+    int i;
+    int a[SIZE] = {1,2,3,4,6};
+    int b[SIZE] = {1,2,3,4,5};
+    int flag_same = 1;
     
-    for(i=0; i<SIZE; i++)
-			 grade[i] = rand() % 100;
-			 
-	  for(i=0; i<SIZE; i++)
-		 	 score[i] = grade[i]; 
-			 
-	  for(i=0; i<SIZE; i++)
-		   printf("score[%d] = %d\n", i, score[i]);
+    for(i=0; i<SIZE; i++){
+			 if(a[i] != b[i])
+			   printf("a의 %d번째 값은 %d, b의 %d번 째 값은 %d 입니다.", i, a[i], i, b[i]);
+				 }
+    
+    /*if(a==b)
+		  printf("배열은 같은 값을 가집니다.\n");
+		else
+		  printf("배열은 다른 값을 가집니다.\n"); */
 
     system("PAUSE");	
     return 0;

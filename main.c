410a17f258c4da5bc1f0ed3daf1c298fc8fce728
sjgift085 <77606778+sjgift085@users.noreply.github.com>
2025-10-30 15:11:53 +0900

    실습 2-3

--- main.c
+++ main.c
@@ -4,15 +4,20 @@
 
 int main(int argc, char *argv[])
 {
-    int i; 
+    int i,average;
+		int sum; 
     int grade[SIZE];
-    printf("5명의 점수를 입력하세요.\n"); 
-  
-    for(i=0; i<SIZE; i++)
-			 scanf("%d", &grade[i]);
     
+    sum = 0;
     for(i=0; i<SIZE; i++)
-			 printf("grade[%d] = %d\n", i, grade[i]);
+    {
+			 printf("학생 성적을 입력하세요: "); 
+			 scanf("%d", &grade[i]);
+			 sum += grade[i];
+    } 
+    
+    average = sum / SIZE; 
+    printf("성적 평균 : %d\n", average); 
   
   system("PAUSE");	
   return 0;

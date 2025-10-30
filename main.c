ce0be7ac73c723c529b37f097e048b6fb073e299
sjgift085 <77606778+sjgift085@users.noreply.github.com>
2025-10-30 15:17:47 +0900

    실습 3-1

--- main.c
+++ main.c
@@ -7,18 +7,17 @@ int main(int argc, char *argv[])
     int i,average;
 		int sum; 
     int grade[SIZE];
+    int score[SIZE];
     
-    sum = 0;
     for(i=0; i<SIZE; i++)
-    {
-			 printf("학생 성적을 입력하세요: "); 
-			 scanf("%d", &grade[i]);
-			 sum += grade[i];
-    } 
-    
-    average = sum / SIZE; 
-    printf("성적 평균 : %d\n", average); 
-  
-  system("PAUSE");	
-  return 0;
+			 grade[i] = rand() % 100;
+			 
+	  for(i=0; i<SIZE; i++)
+		 	 score[i] = grade[i]; 
+			 
+	  for(i=0; i<SIZE; i++)
+		   printf("score[%d] = %d\n", i, score[i]);
+
+    system("PAUSE");	
+    return 0;
 }

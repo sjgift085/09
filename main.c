bf53af1583ae09013fc188b9460056227fa7e9ff
sjgift085 <77606778+sjgift085@users.noreply.github.com>
2025-10-30 11:55:02 +0900

    실습 1

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int grade[5];
+  
+  grade[0] = 10;
+  grade[1] = 20;
+  grade[2] = 30;
+  grade[3] = 40;
+  grade[4] = 50;
+  
+  system("PAUSE");	
+  return 0;
+}

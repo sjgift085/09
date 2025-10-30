f7466f313223dc822577c07773161f0e698a65fe
sjgift085 <77606778+sjgift085@users.noreply.github.com>
2025-10-30 15:08:15 +0900

    실습 2-2

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 5 
 
 int main(int argc, char *argv[])
 {
-  int grade[5];
+    int i; 
+    int grade[SIZE];
+    printf("5명의 점수를 입력하세요.\n"); 
   
-  grade[0] = 10;
-  grade[1] = 20;
-  grade[2] = 30;
-  grade[3] = 40;
-  grade[4] = 50;
-  
-  for(i=0; i<5; i++)
-      printf("grade[%d] = %d\n", i, grade[i]);
+    for(i=0; i<SIZE; i++)
+			 scanf("%d", &grade[i]);
+    
+    for(i=0; i<SIZE; i++)
+			 printf("grade[%d] = %d\n", i, grade[i]);
   
   system("PAUSE");	
   return 0;
